d5e8b4e03dd76c48a8faeb774d277424883b4fbc
radhika2421 <144546481+radhika2421@users.noreply.github.com>
2023-10-07 10:05:49 +0530

    Prime factorization

---
+++ A1-Q10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q10primefactors {
+    public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            System.out.println("Enter a number");
+            int n = scn.nextInt();
+            for (int a = 2; a <= n;a++)
+            {
+                while (n % a == 0)
+                {
+                    n /= a;
+                    System.out.print(a + "*");
+                }
+            }
+        }
+    }
+}
